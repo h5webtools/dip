fe0902bce3f460130e47ae3389c106c977cad61a
canye <canye.wu@jyblife.com>
2017-09-21 20:19:05 +0800

    chore: 修改构建脚本

--- release.sh
+++ release.sh
@@ -3,19 +3,45 @@
 # variables
 basedir=`dirname "$0"`
 workdir=`dirname "$basedir"`
-releasedir="/data1/fe_www/easymock/easy-mock"
+releasedir="$workdir/release"
+appDir="/data1/fe_www/easymock/easy-mock"
 
-# build
-echo "build static"
-cnpm i
-cnpm run build
+# clear release dir
+echo "clear $releasedir"
+if [ -d "$releasedir" ]; then
+    rm -rf "$releasedir"
+fi
 
-# copy files
-echo "copy project files"
-cp -vr $workdir $releasedir
+# create release dir
+echo "create $releasedir"
+mkdir "$releasedir"
 
-echo "entering $releasedir"
+# copy project files
+cp -vr "$workdir/build" "$releasedir/build"
+cp -vr "$workdir/config" "$releasedir/config"
+cp -vr "$workdir/controllers" "$releasedir/controllers"
+cp -vr "$workdir/middlewares" "$releasedir/middlewares"
+cp -vr "$workdir/models" "$releasedir/models"
+cp -vr "$workdir/proxy" "$releasedir/proxy"
+cp -vr "$workdir/public" "$releasedir/public"
+cp -vr "$workdir/util" "$releasedir/util"
+cp -vr "$workdir/views" "$releasedir/views"
+
+cp -v "$workdir/.babelrc" "$releasedir/.babelrc"
+cp -v "$workdir/.eslintrc.js" "$releasedir/.eslintrc.js"
+cp -v "$workdir/.postcssrc.js" "$releasedir/.postcssrc.js"
+cp -v "$workdir/app.js" "$releasedir/app.js"
+cp -v "$workdir/CHANGELOG.md" "$releasedir/CHANGELOG.md"
+cp -v "$workdir/package.json" "$releasedir/package.json"
+cp -v "$workdir/router-config.js" "$releasedir/router-config.js"
+
+# release
+echo "entering $releasedir & scp"
 cd $releasedir
+
+cp -vr "$releaseDir/*" $appDir
+
+cd $appDir
 cd ..
 
 echo "docker-compose up"
