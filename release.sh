59a4525a5e28a4804395ac548e8eba9579cda252
canye <canye.wu@jyblife.com>
2017-09-21 20:23:03 +0800

    chore: 测试ci

--- release.sh
+++ release.sh
@@ -3,46 +3,31 @@
 # variables
 basedir=`dirname "$0"`
 workdir=`dirname "$basedir"`
-releasedir="$workdir/release"
 appDir="/data1/fe_www/easymock/easy-mock"
 
-# clear release dir
-echo "clear $releasedir"
-if [ -d "$releasedir" ]; then
-    rm -rf "$releasedir"
-fi
-
-# create release dir
-echo "create $releasedir"
-mkdir "$releasedir"
-
 # copy project files
-cp -vr "$workdir/build" "$releasedir/build"
-cp -vr "$workdir/config" "$releasedir/config"
-cp -vr "$workdir/controllers" "$releasedir/controllers"
-cp -vr "$workdir/middlewares" "$releasedir/middlewares"
-cp -vr "$workdir/models" "$releasedir/models"
-cp -vr "$workdir/proxy" "$releasedir/proxy"
-cp -vr "$workdir/public" "$releasedir/public"
-cp -vr "$workdir/util" "$releasedir/util"
-cp -vr "$workdir/views" "$releasedir/views"
+cp -vr "$workdir/build" "$appDir/build"
+cp -vr "$workdir/config" "$appDir/config"
+cp -vr "$workdir/controllers" "$appDir/controllers"
+cp -vr "$workdir/middlewares" "$appDir/middlewares"
+cp -vr "$workdir/models" "$appDir/models"
+cp -vr "$workdir/proxy" "$appDir/proxy"
+cp -vr "$workdir/public" "$appDir/public"
+cp -vr "$workdir/util" "$appDir/util"
+cp -vr "$workdir/views" "$appDir/views"
 
-cp -v "$workdir/.babelrc" "$releasedir/.babelrc"
-cp -v "$workdir/.eslintrc.js" "$releasedir/.eslintrc.js"
-cp -v "$workdir/.postcssrc.js" "$releasedir/.postcssrc.js"
-cp -v "$workdir/app.js" "$releasedir/app.js"
-cp -v "$workdir/CHANGELOG.md" "$releasedir/CHANGELOG.md"
-cp -v "$workdir/package.json" "$releasedir/package.json"
-cp -v "$workdir/router-config.js" "$releasedir/router-config.js"
+cp -v "$workdir/.babelrc" "$appDir/.babelrc"
+cp -v "$workdir/.eslintrc.js" "$appDir/.eslintrc.js"
+cp -v "$workdir/.postcssrc.js" "$appDir/.postcssrc.js"
+cp -v "$workdir/app.js" "$appDir/app.js"
+cp -v "$workdir/CHANGELOG.md" "$appDir/CHANGELOG.md"
+cp -v "$workdir/package.json" "$appDir/package.json"
+cp -v "$workdir/router-config.js" "$appDir/router-config.js"
 
 # release
-echo "entering $releasedir & scp"
-cd $releasedir
-
-cp -vr "$releaseDir/*" $appDir
-
+echo "entering $appDir"
 cd $appDir
 cd ..
 
 echo "docker-compose up"
-docker-compose up -d
+/usr/local/bin/docker-compose up -d
