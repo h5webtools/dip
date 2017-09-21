21d469b20f4a71f349cf6787a1727d2a84d17878
canye <canye.wu@jyblife.com>
2017-09-21 14:33:32 +0800

    chore: 修改release.sh

--- release.sh
+++ release.sh
@@ -13,7 +13,8 @@ cnpm run build
 echo "copy project files"
 cp -vr $workdir $releasedir
 
-echo "entering $releasedir & docker-compose up"
+echo "entering $releasedir"
 cd $releasedir
 
+echo "docker-compose up"
 docker-compose up -d
