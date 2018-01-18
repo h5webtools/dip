2961c683c40f438e9fd2993735c0b214bd43a920
canye <canye.wu@jyblife.com>
2018-01-18 21:46:28 +0800

    feat: 修改接口列表menu为tree

--- client/src/components/list/Menu.vue
+++ client/src/components/list/Menu.vue
@@ -4,18 +4,26 @@
       <el-menu-item-group :class="{showSearch: isShowSearch}">
         <template slot="title">
           <span @click="handleClickGroup">组列表</span>
-          <search @query="onQuery" v-model="query" placeholder="请输入分组名称" size="small" id="search-group"></search>
+          <search @query="handleQuery" v-model="query" placeholder="请输入分组名称" size="small" id="search-group"></search>
           <i class="el-icon-plus title-icon" @click="handleClickShowDialog"></i>
           <i class="el-icon-search title-icon" @click="showSearch()"></i>
         </template>
-        <el-menu-item v-for="group in groupList"
-                      :index="group._id"
-                      class="group-item"
-                      @click="handleClickGroup(group)"
-                      :key="group._id">
-          <i class="el-icon-document" @click.stop="showGroupDoc(group)"></i>
-          <span>{{group.name}}</span>
-        </el-menu-item>
+        <el-tree
+          class="menu-tree-wrap"
+          :data="treeGroup"
+          :props="defaultProps"
+          :current-node-key="currentNodeKey"
+          :default-expanded-keys="expandKey"
+          :expand-on-click-node="false"
+          :highlight-current="true"
+          :filter-node-method="filterNode"
+          :indent="20"
+          :render-content="renderContent"
+          node-key="key"
+          accordion
+          ref="menuTreeWrap"
+          @node-click="handleNodeClick">
+        </el-tree>
       </el-menu-item-group>
     </el-menu>
     <create-group-dialog
@@ -35,28 +43,54 @@ export default {
   computed: {
     groupList () {
       return this.$store.state.groups.filter(g => g.name.toLowerCase().indexOf(this.query.toLowerCase()) >= 0)
+    },
+    treeGroup() {
+      return this.$store.getters.treeGroups;
     }
   },
   data () {
     return {
+      currentNodeKey: this.$route.params.groupId || '',
+      expandKey: [this.$route.params.groupId || ''],
       showCreateDialog: false,
       isShowSearch: false,
-      query: ''
+      query: '',
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
     }
   },
   methods: {
+    renderContent(h, { node, data, store }) {
+      return (
+        <span>
+          <span>
+            <span>{node.label}</span>
+          </span>
+          <span title="接口文档" class="tree-node-doc">
+            <i class="el-icon-document" on-click={ (ev) => this.showGroupDoc(ev, node.key) }></i>
+          </span>
+        </span>
+      );
+    },
+    handleQuery(val) {
+      this.query = val;
+      this.$refs.menuTreeWrap.filter(val);
+    },
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.label.indexOf(value) !== -1;
+    },
     showSearch () {
       this.isShowSearch = true
       window.setTimeout(() => {
         document.querySelector('#search-group input').focus()
       })
     },
-    onQuery (val) {
-      this.query = val
-    },
-    handleClickGroup (group) {
-      if (group && group._id) {
-        this.$router.replace(`/list/${group._id}`)
+    handleNodeClick(group) {
+      if (group && group.key) {
+        this.$router.replace(`/list/${group.key}`)
       } else {
         this.$router.replace('/')
       }
@@ -72,11 +106,12 @@ export default {
     handleClickShowDialog () {
       this.showCreateDialog = true
     },
-    showGroupDoc (group) {
+    showGroupDoc (ev, key) {
+      ev.stopPropagation();
       this.$router.push({
         name: 'GroupDoc',
         params: {
-          groupId: group._id
+          groupId: key
         }
       })
     }
@@ -88,6 +123,53 @@ export default {
   width: 288px;
   min-width: 288px;
   background-color: #eef1f6;
+
+  .menu-tree-wrap {
+    background-color: #eef1f6;
+    border: none;
+
+    & > .el-tree-node {
+      & > .el-tree-node__content {
+        padding-left: 20px !important;
+      }
+    }
+
+    .tree-node-doc {
+      float: right;
+      margin-right: 10px;
+      display: none;
+    }
+
+    .el-tree-node__content {
+      height: 56px;
+      line-height: 56px;
+      font-size: 14px;
+      color: #48576a;
+      padding-right: 20px;
+      cursor: pointer;
+      position: relative;
+      transition: border-color .3s, background-color .3s, color .3s;
+      box-sizing: border-box;
+      white-space: nowrap;
+
+      &:hover {
+        background-color: #d1dbe5;
+
+        .tree-node-doc {
+          display: block;
+        }
+      }
+    }
+  }
+
+  .el-tree--highlight-current {
+    .el-tree-node.is-current {
+      & > .el-tree-node__content {
+        background-color: #eef1f6;
+        color: #20a0ff;
+      }
+    }
+  }
 
   .el-menu-item-group__title {
     padding: 15px 20px 0 20px;
