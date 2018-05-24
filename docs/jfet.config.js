994c8fe5f380c8ba6c6bc92663e4a71a3b1944ec
canye <canye.wu@jyblife.com>
2018-05-24 10:36:54 +0800

    chore: 修改文档构建配置

--- docs/jfet.config.js
+++ docs/jfet.config.js
@@ -8,6 +8,8 @@ module.exports = {
   doc() {
     return {
       name: pkg.name,
+      title: '数据接口平台文档',
+      desc: '数据接口平台的使用介绍，包含创建/编辑接口，Mock，分组，权限等内容。',
       token: '21232F297A57A5A743894A0E4A801FC3',
       uploadUrl: 'http://doc.fe.jyb.com/api/upload'
     };
